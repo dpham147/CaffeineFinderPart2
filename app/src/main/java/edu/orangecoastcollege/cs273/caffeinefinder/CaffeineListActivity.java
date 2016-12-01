5725e3182834aeab57044833fe024a5cbd5c37e8
Pham <dpham147@student.cccd.edu>
2016-12-01 15:39:17 -0800

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/caffeinefinder/CaffeineListActivity.java
@@ -0,0 +1,166 @@
+package edu.orangecoastcollege.cs273.caffeinefinder;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+public class CaffeineListActivity extends AppCompatActivity
+        implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private static final int FINE_LOCATION_REQUEST_CODE = 100;
+    private DBHelper db;
+    private List<CaffeineLocation> mAllLocationsList;
+    private ListView locationsListView;
+    private LocationListAdapter locationListAdapter;
+    private GoogleMap mMap;
+
+    // Member variable to access the Google Play services (LOCATOR SERVICES)
+    private GoogleApiClient mGoogleApiClient;
+    // Member variable to store LocationRequest (how often to update)
+    private LocationRequest mLocationRequest;
+    // Member variable to store current location
+    private Location currentLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caffeine_list);
+
+        deleteDatabase(DBHelper.DATABASE_NAME);
+        db = new DBHelper(this);
+        db.importLocationsFromCSV("locations.csv");
+
+        mAllLocationsList = db.getAllCaffeineLocations();
+        locationsListView = (ListView) findViewById(R.id.locationsListView);
+        locationListAdapter = new LocationListAdapter(this, R.layout.location_list_item, mAllLocationsList);
+        locationsListView.setAdapter(locationListAdapter);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.caffeineMapFragment);
+        mapFragment.getMapAsync(this);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        // Define the interval for updates
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10000)
+                .setFastestInterval(1000);
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_REQUEST_CODE);
+        }
+        currentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+        handleNewLocation(currentLocation);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e("CaffeineFinder", "Connection to Google Play was suspended.");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e("CaffeineFinder", "Connection to Google Play failed.");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
+
+    private void handleNewLocation(Location location)
+    {
+
+        mMap.clear();
+        // Change value of myLocation to new location
+        currentLocation = location;
+        // Add special marker (blue) for "my" location
+        //MBCC Building Lat/Lng (MBCC 135)  33.671028, -117.911305
+        LatLng myCoordinate = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        mMap.addMarker(new MarkerOptions()
+                .position(myCoordinate)
+                .title("Current Location")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.my_marker)));
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(myCoordinate).zoom(14.0f).build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        mMap.moveCamera(cameraUpdate);
+
+        // Add normal markers for all caffeine locations
+        for (CaffeineLocation caffeineLocation : mAllLocationsList) {
+            LatLng coordinate = new LatLng(caffeineLocation.getLatitude(), caffeineLocation.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(coordinate).title(caffeineLocation.getName()));
+        }
+    }
+
+    public void findClosestCaffeine(View view)
+    {
+        double minDistance = Double.MAX_VALUE;
+        CaffeineLocation closestCaffeineLocation = null;
+        for (CaffeineLocation caffeineLocation : mAllLocationsList)
+        {
+            Location location = new Location("");
+            location.setLatitude(caffeineLocation.getLatitude());
+            location.setLongitude(caffeineLocation.getLongitude());
+            double distance = currentLocation.distanceTo(location);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closestCaffeineLocation = caffeineLocation;
+            }
+        }
+        // Launch new intent to details activity
+//        Intent detailsIntent = new Intent(this, CaffeineDetailsActivity.class);
+//        detailsIntent.putExtra("ClosestCaffeine", closestCaffeineLocation);
+//        startActivity(detailsIntent);
+
+    }
+}
